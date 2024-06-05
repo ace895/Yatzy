ccf536d5a16080a904a5a6c90b85cf70ffb2d216
Vivian Quach <nquac016@uottawa.ca>
2024-06-05 15:32:07 -0400

    Changing functions into modules

--- public/assets/dice.js
+++ public/assets/dice.js
@@ -1,15 +1,22 @@
-function dice_roll(){
-    return Math.floor(Math.random() * 6) + 1; 
-}
-
-function multi_dice_roll(num){
-    var results = []; 
-    for(let i = 0; i < num; i++){
-        results.push(dice_roll()); 
+var Dice = (function(){
+    var entity = {
+        "history": []
+      };    
+    entity.dice_roll = function() {
+        var roll = Math.floor(Math.random() * 6) + 1;
+        this.history.push(roll);
+        console.log(roll);
+        return roll; 
     }
+    entity.multi_dice_roll = function() {
+        var results = []; 
+        for(let i = 0; i < num; i++){
+            var roll = Math.floor(Math.random() * 6) + 1;
+            results.push(roll); 
+            this.history.push(roll);
+        }
     return results; 
-}
-console.log(dice_roll());
+    }
+})(); 
 
-//console.log(multi_dice_roll(3)); // An example of how this function is used
 
