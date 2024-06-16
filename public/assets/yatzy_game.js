65157ea74a2b5ae48a220a44fd0de9645041a010
Anika Pathak <anika.pathak@gmail.com>
2024-06-15 23:30:13 -0400

    Fix reroll mechanism

--- public/assets/yatzy_game.js
+++ public/assets/yatzy_game.js
@@ -5,7 +5,7 @@ var Game = (function() {
     const MAX_REROLLS = 2;
     var this_game = {
         "roll_num": 0, //Stores current roll
-        "rerolls": new Array(NUM_OF_DICE).fill(0), //Stores number of rerolls for each dice
+        "rerolls": 0, //Stores number of rerolls done for current roll
         "dice": new Array(NUM_OF_DICE) //Stores value of each dice
     }
 
@@ -34,13 +34,15 @@ var Game = (function() {
         this.dice = Dice.multi_dice_roll(NUM_OF_DICE); //Roll all dice
     }
 
-    //Reroll dice n if it has less than 2 rerolls
-    this_game.reroll = function(n) {
-        if (this.rerolls[n] == MAX_REROLLS) {
+    //Takes integer array of dice to reroll and rerolls them
+    this_game.reroll = function(dice_to_reroll) {
+        if (this.rerolls == MAX_REROLLS) {
             return;
         }
-        this.dice[n] = Dice.dice_roll();
-        this.rerolls[n] ++;
+        for(var i = 0; i < dice_to_reroll.length; i ++) {
+            this.dice[i] = Dice.dice_roll();
+        }
+        this.rerolls ++;
     }
 
     return this_game;
