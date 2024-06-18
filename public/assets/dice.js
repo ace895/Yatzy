56c87b463305bff5300918742669be92046ccbc9
Anika Pathak <anika.pathak@gmail.com>
2024-06-18 12:13:13 -0400

    Reorganize code

--- public/assets/dice.js
+++ public/assets/dice.js
@@ -1,20 +1,19 @@
-export var Dice = (function(){
-    var entity = {
-        "history": []
-      };    
-    entity.dice_roll = function() {
-        var roll = Math.floor(Math.random() * 6) + 1;
+var Dice = {
+    history: [],
+
+    dice_roll() {
+        const roll = Math.floor(Math.random() * 6) + 1;
         this.history.push(roll);
-        console.log(roll);
-        return roll; 
-    }
-    entity.multi_dice_roll = function(num) {
-        var results = []; 
-        for(let i = 0; i < num; i++){
-            var roll = Math.floor(Math.random() * 6) + 1;
-            results.push(roll); 
+        return roll;
+    },
+
+    multi_dice_roll(num) {
+        results = [];
+        for (let i = 0; i < num; i++) {
+            const roll = Math.floor(Math.random() * 6) + 1;
+            results.push(roll);
             this.history.push(roll);
         }
-    return results; 
+        return results;
     }
-})(); 
+};
--- public/assets/yatzy_game.js
+++ public/assets/yatzy_game.js
@@ -1,48 +1,23 @@
-import { Dice } from './dice.js'; 
+var Game = {
+    dice: new Array(5),
+    roll_num: 0,
+    rerolls: 0,
 
-class Game{
-    constructor(){
-        const dice = new Array(5); 
-        const roll_num  = 0; 
-        const rerolls = 0; 
-    }
-    get_roll_num = function() {
-        return this.roll_num;
-    }
-    get_dice = function() {
-        return this.dice;
-    }
-    get_rerolls = function () {
-        return this.rerolls;
-    }
-    set_rerolls = function(){
-        rerolls = 0; 
-    }
-    //Start (or restart) game, initialize variables
-    start_game = function() {
-        this.roll_num = 0;
-        this.dice = new Array(5);
-        this.rerolls = new Array(5).fill(0);
-    }
     //Roll all dice
-    roll = function() {
+    roll() {
         this.roll_num ++;
-        this.rerolls.fill(0); //Reset rerolls
+        this.rerolls = 0; //Reset rerolls
         this.dice = Dice.multi_dice_roll(5); //Roll all dice
-    }
+    },
+
     //Takes integer array of dice to reroll and rerolls them
-    reroll = function(dice_to_reroll) {
-        if (this.rerolls >= 2) {
+    reroll(dice_to_reroll) {
+        if (this.rerolls >= 3) {
             return;
         }
-        let count = 0; 
         for(var i = 0; i < dice_to_reroll.length; i ++) {
-            if (dice_to_reroll(count) === i ){
-                this.dice[i] = Dice.dice_roll();
-                count++;
-            }
+            this.dice[dice_to_reroll[i]] = Dice.dice_roll();
         }
         this.rerolls ++;
     }
 }
-
