8917f4b7573a9a25ff2f7d802ab33c5995e0e837
Anika Pathak <anika.pathak@gmail.com>
2024-07-28 23:06:17 -0400

    Add user table sorting

--- public/assets/yatzy_admin.js
+++ public/assets/yatzy_admin.js
@@ -1,16 +1,21 @@
+var users;
+
 //Load user information to table
 window.onload = async function() {
     //Get users from database
-    var users = await callWithoutParam("getUsers");
+    users = await callWithoutParam("getUsers");
     var table = document.getElementById("user-table").getElementsByTagName("tbody")[0];
                             
     for (var user of users) {
         //Get user's high score and leaderboard rank
         var score = await callWithParam("getScores", user.username);
         var highScore = score.length > 0 ? score[0].score : "No scores";
-        var leaderboard = await callWithoutParam("getLeaderboard");
-        var rankEntry = leaderboard.find(entry => entry.username === user.username);
-        var rank = rankEntry ? rankEntry.leaderboard_rank : "N/A";
+        var leaderboard = await callWithoutParam("leaderboard");
+        var rankEntry = leaderboard == null ? null : leaderboard.find(entry => entry.username === user.username);
+        var rank = rankEntry != null && rankEntry ? rankEntry.leaderboard_rank : "N/A";
+        user["rank"] = rank;
+        user["highScore"] = highScore;
+
         var deleteButton = "<button class='small delete-btn' onclick='deleteUser(\"" + user.username + "\")'><b>Delete<b></button>"
         var infoButton = "<button class='small' onclick='getUserScores(\"" + user.username + "\")'><b>View Scores<b></button>"
 
@@ -18,8 +23,8 @@ window.onload = async function() {
                             "</td><td class='info-td'>" + user.username +
                             "</td><td class='info-td'>" + user.registration_date + 
                             "</td><td class='info-td'>" + user.last_login +
-                            "</td><td class='info-td'>" + highScore +
-                            "</td><td class='info-td'>" + rank + 
+                            "</td><td class='info-td'>" + user.highScore +
+                            "</td><td class='info-td'>" + user.rank + 
                             "</td><td class='blank-td'>" + infoButton + 
                             "</td><td class='blank-td'>" + deleteButton + "</td></tr>";
     }
@@ -49,4 +54,94 @@ function getUserScores(username) {
     }
 
     table.style.visibility = "visible";
+}
+
+function rankByName() {
+    users.sort((a, b) => {
+        if (a.first_name + " " + a.last_name < b.first_name + " " + b.last_name) {
+            return -1;
+        }
+        if (a.first_name + " " + a.last_name > b.first_name + " " + b.last_name) {
+            return 1;
+        }
+        return 0;
+    });
+    loadUsersTable();
+}
+
+function rankByUsername() {
+    users.sort((a, b) => {
+        if (a.username < b.username) {
+            return -1;
+        }
+        if (a.username > b.username) {
+            return 1;
+        }
+        return 0;
+    });
+    loadUsersTable();
+}
+
+function rankByRegDate() {
+    users.sort((a, b) => {
+        let dateA = new Date(a.registration_date);
+        let dateB = new Date(b.registration_date);
+        return dateB - dateA; 
+    });
+    loadUsersTable();
+}
+
+function rankByLastLogin() {
+    users.sort((a, b) => {
+        let dateA = new Date(a.last_login);
+        let dateB = new Date(b.last_login);
+        return dateB - dateA; 
+    });
+    loadUsersTable();
+}
+
+
+function rankByHighscore() {
+    users.sort((a, b) => {
+        if (a.rank == "N/A" || a.rank < b.rank) {
+            return -1;
+        }
+        if (b.rank == "N/A" || a.rank > b.rank) {
+            return 1;
+        }
+        return 0;
+    });
+    loadUsersTable();
+}
+
+function rankByRank() {
+    users.sort((a, b) => {
+        if (a.rank == "N/A" || a.rank < b.rank) {
+            return -1;
+        }
+        if (b.rank == "N/A" || a.rank > b.rank) {
+            return 1;
+        }
+        return 0;
+    });
+    loadUsersTable();
+}
+
+function loadUsersTable() {
+    var table = document.getElementById("user-table").getElementsByTagName("tbody")[0];
+    table.innerHTML = ""; 
+
+    for (var user of users) {
+        var deleteButton = "<button class='small delete-btn' onclick='deleteUser(\"" + user.username + "\")'><b>Delete<b></button>";
+        var infoButton = "<button class='small' onclick='getUserScores(\"" + user.username + "\")'><b>View Scores<b></button>";
+
+        table.innerHTML += "<tr><td onclick=\"rankByName()\" class='info-td'>" + user.first_name + " " + user.last_name +
+            "</td><td onclick=\"rankByUsername()\" class='info-td'>" + user.username +
+            "</td><td onclick=\"rankByRegDate()\" class='info-td'>" + user.registration_date +
+            "</td><td onclick=\"rankByLastLogin()\" class='info-td'>" + user.last_login +
+            "</td><td onclick=\"rankByHighscore()\" class='info-td'>" + user.highScore +
+            "</td><td onclick=\"rankByRank()\" class='info-td'>" + user.rank +
+            "</td><td class='blank-td'>" + infoButton +
+            "</td><td class='blank-td'>" + deleteButton + "</td></tr>";
+    }
 }
